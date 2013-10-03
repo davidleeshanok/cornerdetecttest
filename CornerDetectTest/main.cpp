e464da73c46ef2a1c676ef646f601ee6cc4c04c6
David Lee-Shanok <David@Davids-MacBook-Air.local>
2013-10-02 23:06:21 -0400

    Initial Commit

---
+++ CornerDetectTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CornerDetectTest
+//
+//  Created by David Lee-Shanok on 2013-10-02.
+//  Copyright (c) 2013 David Lee-Shanok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
